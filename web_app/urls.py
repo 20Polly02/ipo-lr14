e2fc861fd52bcf3974e1cae58412f694a347532d
Polly <polialekseevna80@gmail.com>
2025-06-10 21:50:10 +0300

    ipo_lr15

--- web_app/urls.py
+++ web_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home_page, about_autor, home_goods_store
+from .views import home_page, about_autor, home_goods_store, speciality, speciality_id, speciality_found
 
 urlpatterns = [
     path('', home_page, name='home_page'),
     path('about_autor', about_autor, name='about_autor'),
     path('home_goods_store', home_goods_store, name='home_goods_store'),
+    path('spec', speciality, name='speciality'),
+    path('spec/<int:id>/', speciality_id, name='speciality_id'),
+    path('speciality_found', speciality_found, name='speciality_found'),
 ]
--- web_app/views.py
+++ web_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import json
 def home_page(request):
     return render(request,'home_page.html')
 
@@ -7,4 +8,53 @@ def about_autor(request):
 
 def home_goods_store(request):
     return render(request,'home_goods_store.html')
+def speciality(request):
+    specialities=[]
+    with open("data/dump.json", 'r', encoding='utf-8') as file:  
+        info_file = json.load(file)
+    for specialty in info_file:  
+        if specialty.get("model") == "data.specialty": 
+            speciality = {
+                "code": specialty["fields"].get("code"),
+                "pk": specialty.get("pk"), 
+                "title" : specialty["fields"].get("title"),  
+                "c_type": specialty["fields"].get("c_type")  
+            }
+            specialities.append(speciality)
+    return render(request,'speciality.html',{'specialities':specialities})
+def speciality_found(request):
+    c = request.GET.get('c')
+    speciality = []  
+    with open("data/dump.json", 'r', encoding='utf-8') as file:
+        read_file = json.load(file)
+        for specialty in read_file:
+            if specialty.get("model") == "data.specialty":
+                if specialty["fields"].get("code") == c:
+                    specialty_f = {
+                        "code": specialty["fields"].get("code"),
+                        "pk": specialty.get("pk"),
+                        "title": specialty["fields"].get("title"),
+                        "educational": specialty["fields"].get("c_type"),
+                    }
+                    speciality.append(specialty_f)  
+
+    return render(request, "speciality_found.html", {'speciality': speciality})
+
+def speciality_id(request,id):
+    speciality = []  
+    with open("data/dump.json", 'r', encoding='utf-8') as file:
+        read_file = json.load(file)
+        for specialty in read_file:
+            if specialty.get("model") == "data.specialty":
+                if int(specialty.get("pk")) == int(id):
+                    specialty_i= {
+                        "code": specialty["fields"].get("code"),
+                        "pk": specialty.get("pk"),
+                        "title": specialty["fields"].get("title"),
+                        "educational": specialty["fields"].get("c_type"),
+                    }
+                    speciality.append(specialty_i)  
+
+    return render(request, "speciality_found.html", {'speciality': speciality}) 
+
 # Create your views here.
